97a036a757146584d321ba22c7845eded47978ec
Melissa Simpson <melissacraxton@gmail.com>
2023-10-11 07:28:41 +0100

    Add links, specify language, add subject, control the category

--- main.py
+++ main.py
@@ -6,24 +6,32 @@ import requests
 #  https://newsapi.org/
 api_key = '58df71ff4ef1421893045a627b52d7db'
 
+topic = 'tesla'
+
 #  This is the endpoint
-url = 'https://newsapi.org/v2/everything?q=tesla&'\
-    'from=2023-09-09&sortBy=publishedAt&'\
-    'apiKey=58df71ff4ef1421893045a627b52d7db'
+url = f'https://newsapi.org/v2/everything?q={topic}&' \
+      'from=2023-09-11&sortBy=publishedAt&' \
+      'apiKey=58df71ff4ef1421893045a627b52d7db&' \
+      'language=en'
 
 #  Make a request
 request = requests.get(url)
 # The above url returns a string - print(type(content))
+
 # use .json to convert it to a dict
 content = request.json()
-
+print(content)
 # Access the article title and description
 body = ''
-for article in content['articles']:
+for article in content['articles'][0:20]:
     # print(article['title'])
     # print(article['author'])
     if article['title'] is not None:
-        body = body + article['title'] + "\n" + article['url'] + 2* '\n'
+        body = "Subject: Today's news" + '\n' \
+               + body + article['title'] + "\n" \
+               + article['publishedAt'] + "\n"  \
+               + article['url'] \
+               + '\n' + 2 * '\n'
 
 body = body.encode('utf-8')
 send_email(message=body)
